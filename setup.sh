4b5398f293c9b083ffa131f41e16f77ea63004a9
shenmishajing <shenmishajing@Gmail.com>
2020-12-18 16:27:16 +0800

    remove .clang-format and .gdbinit files

--- setup.sh
+++ setup.sh
@@ -1,14 +1,10 @@
-rm -rf $HOME/.clang-format
 rm -rf $HOME/.condarc
-rm -rf $HOME/.gdbinit
 rm -rf $HOME/.gitconfig
 rm -rf $HOME/.gitignore_global
 rm -rf $HOME/.tmux.conf
 rm -rf $HOME/.pip
 
-ln -s `pwd`/Setting-for-Mac/.clang-format $HOME/
 ln -s `pwd`/Setting-for-Mac/.condarc $HOME/
-ln -s `pwd`/Setting-for-Mac/.gdbinit $HOME/
 ln -s `pwd`/Setting-for-Mac/.gitconfig $HOME/
 ln -s `pwd`/Setting-for-Mac/.gitignore_global $HOME/
 ln -s `pwd`/Setting-for-Mac/.tmux.conf $HOME/
