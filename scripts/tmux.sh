33d87caef7392b58dc2beec80ba50929320cf549
shenmishajing <shenmishajing@gmail.com>
2023-11-17 10:14:31 +0800

    fix tmux.config bug

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -1,7 +1,7 @@
 rm -rf $HOME/.config/tmux
 rm -rf $HOME/.tmux.conf
 
-ln -s `pwd`/Setting-for-Mac/.tmux.conf $HOME/
+ln -s `pwd`/.tmux.conf $HOME/
 
 git clone https://github.com/tmux-plugins/tpm $HOME/.config/tmux/plugins/tpm
 bash $HOME/.config/tmux/plugins/tpm/bin/install_plugins
